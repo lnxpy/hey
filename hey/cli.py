9bce0fc5e8f621f20fac48d03ca2b97da2225de5
Sadra Yahyapour <lnxpylnxpy@gmail.com>
2023-04-18 14:38:05 +0330

    guide information added

--- hey/cli.py
+++ hey/cli.py
@@ -2,18 +2,46 @@
 import argparse
 import sys
 
+from constants.informations import APPLICATION_DESCRIPTION, EPILOG_DESCRIPTION, INSTALLATION_GUIDE, VERSION_INFO
+from validators.credentials import credentials_validator
+
+from hey import __version__
+
+parser = argparse.ArgumentParser(
+    description=APPLICATION_DESCRIPTION + '\n\r\n\r' + INSTALLATION_GUIDE,
+    epilog=EPILOG_DESCRIPTION,
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+)
+
+parser.add_argument(
+    '--version',
+    action='version',
+    version=VERSION_INFO.format(__version__),
+)
+
+parser.add_argument(
+    'ask',
+    type=str,
+    nargs='*',
+    help='ask what you need',
+)
+
+parser.add_argument(
+    "--setup",
+    type=credentials_validator,
+    help="your mindsdb credentials",
+)
+
 
 def main():
-    """Console script for hey."""
-    parser = argparse.ArgumentParser()
-    parser.add_argument('_', nargs='*')
     args = parser.parse_args()
 
-    print("Arguments: " + str(args._))
-    print("Replace this message by putting your code into "
-          "hey.cli.main")
+    # TODO: check if credentials are valid
+    # TODO: check for config file (if exists, override)
+
+    # TODO: deal with ask option
     return 0
 
 
 if __name__ == "__main__":
-    sys.exit(main())  # pragma: no cover
+    sys.exit(main())
