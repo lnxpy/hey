b1da0b3504d7ff0a650562b3bbe4fe52c6720efb
Sadra Yahyapour <lnxpylnxpy@gmail.com>
2023-04-18 14:37:51 +0330

    guide information added

---
+++ constants/informations.py
@@ -0,0 +1,12 @@
+# package description for argparse
+APPLICATION_DESCRIPTION = 'Hey is an AI-powered pair programming assistant that helps you speed up your productivity.'
+
+# link to installation guide
+INSTALLATION_GUIDE = '[-] Check out https://imsadra/introducing-hey-your-ai-powered-pair-programming-friend/ for ' \
+                     'installation guide. '
+
+# epilog information
+EPILOG_DESCRIPTION = '[>] Hey is designed for Hashnode X MindsDB hackathon.'
+
+# `--version` option output pattern
+VERSION_INFO = 'You\'re using Hey v{}!'
