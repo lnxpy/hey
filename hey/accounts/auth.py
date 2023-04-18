d8ccc137b621ad51af63c5588bc9cce34a87ab4e
Sadra Yahyapour <lnxpylnxpy@gmail.com>
2023-04-18 18:20:50 +0330

    base user authorization class created

--- hey/accounts/auth.py
+++ hey/accounts/auth.py
@@ -1,8 +1,17 @@
+from typing import Union
+
+import mindsdb_sdk
+from mindsdb_sdk.server import Server
+from requests.exceptions import HTTPError, ConnectionError
+
+from hey.constants.service import MINDSDB_HOST
+from hey.exceptions.auth import CredentialsError
+from hey.exceptions.connection import NetworkError
 
 
-class Authentication:
+class User:
     """
-    MindsDB authentication manager class
+    MindsDB user auth manager class
     """
 
     def __init__(self, email: str, password: str) -> None:
@@ -15,5 +24,24 @@ class Authentication:
         self.email = email
         self.password = password
 
-    def is_authenticated(self):
-        return True
+        self.is_authenticated: bool = False
+
+    def authenticate(self) -> Union[Server, None]:
+        """
+        authorizes the email and password with MindsDB's host
+        Returns:
+            user object
+        """
+        try:
+            user = mindsdb_sdk.connect(
+                MINDSDB_HOST,
+                login=self.email,
+                password=self.password,
+            )
+        except HTTPError:
+            raise CredentialsError('Email or password is incorrect. Make sure to enter the right credentials.')
+        except ConnectionError:
+            raise NetworkError('Make sure you have access to the connection and try again.')
+
+        self.is_authenticated = True
+        return user
