f227067823bb818a708164e4a55b72ccdfd68f25
Sadra Yahyapour <lnxpylnxpy@gmail.com>
2023-04-22 12:43:43 +0330

    auth operations implemented + --auth option refactored to --set-password

--- hey/cli.py
+++ hey/cli.py
@@ -1,11 +1,16 @@
-"""Console script for hey."""
 import argparse
+import os
 import sys
 
 from hey import __version__
 from hey.accounts.auth import User
 from hey.constants.informations import APPLICATION_DESCRIPTION, EPILOG_DESCRIPTION, INSTALLATION_GUIDE, VERSION_INFO
+from hey.constants.service import SERVICE_NAME
+from hey.exceptions.system import EmailEnvVarNotExists, BrokenCredentials
 from hey.validators.credentials import credentials_validator
+
+import keyring
+
 
 parser = argparse.ArgumentParser(
     description=APPLICATION_DESCRIPTION + '\n\r\n\r' + INSTALLATION_GUIDE,
@@ -21,28 +26,48 @@ parser.add_argument(
 
 parser.add_argument(
     'ask',
-    type=str,
     nargs='*',
     help='ask what you need',
 )
 
 parser.add_argument(
-    "--auth",
-    type=credentials_validator,
-    help="your mindsdb authenticating credentials in <email>:<password> pattern (e.g. abc@def.com:testpass)",
+    '--set-password',
+    type=str,
+    help='set your mindsdb account password',
+    metavar='',
 )
 
 
 def main():
     args = parser.parse_args()
 
-    if args.auth:
-        # TODO: Refactor User class to Connection class
-        user = User(**args.auth)
-        user.authenticate()
+    if args.set_password:
+        email_address = os.environ.get('HEY_EMAIL_ADDRESS')
+        if email_address:
+            keyring.set_password(
+                service_name=SERVICE_NAME.lower(),
+                username=email_address,
+                password=args.set_password,
+            )
+        else:
+            raise EmailEnvVarNotExists('Make sure you have defined HEY_EMAIL_ADDRESS environment variable properly.')
 
-    # TODO: check for config file (if exists, override)
-    # TODO: deal with ask option
+    credentials = keyring.get_credential(
+        service_name=SERVICE_NAME.lower(),
+        username=os.environ.get('HEY_EMAIL_ADDRESS'),
+    )
+
+    if not credentials:
+        raise BrokenCredentials(
+            'Make sure you have set your HEY_EMAIL_ADDRESS and password via --set-password.'
+        )
+
+    if args.ask:
+        # TODO: Refactor User class to Connection class
+        user = User(
+            email=credentials.username,
+            password=credentials.password
+        )
 
     return 0
 
