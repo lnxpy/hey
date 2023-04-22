520f0cb02045d7394c0cde5c307394e7cdabbe08
Sadra Yahyapour <lnxpylnxpy@gmail.com>
2023-04-22 12:44:10 +0330

    VERSION_INFO value refactored

--- hey/constants/informations.py
+++ hey/constants/informations.py
@@ -1,12 +1,18 @@
 # package description for argparse
-APPLICATION_DESCRIPTION = 'Hey is an AI-powered pair programming assistant that helps you speed up your productivity.'
+APPLICATION_DESCRIPTION = 'Hey is your AI-powered pair programming friend that helps you speed up your productivity.'
 
 # link to installation guide
 INSTALLATION_GUIDE = '[-] Check out https://imsadra/introducing-hey-your-ai-powered-pair-programming-friend/ for ' \
                      'installation guide. '
 
 # epilog information
-EPILOG_DESCRIPTION = '[>] Hey is designed for Hashnode X MindsDB hackathon.'
+EPILOG_DESCRIPTION = '[-] Hey is designed for Hashnode X MindsDB hackathon.'
 
 # `--version` option output pattern
-VERSION_INFO = 'You\'re using Hey v{}!'
+VERSION_INFO = '''
+  _  _          _
+ | || |___ _  _| |
+ | __ / -_| || |_|
+ |_||_\___|\_, (_)
+           |__/ --> v{}
+'''
