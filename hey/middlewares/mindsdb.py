d181c5a66dad3b50c6e55d370464d802022e77e3
Sadra Yahyapour <lnxpylnxpy@gmail.com>
2023-04-22 18:57:06 +0330

    using rich.Markdown for response

--- hey/middlewares/mindsdb.py
+++ hey/middlewares/mindsdb.py
@@ -1,39 +1,60 @@
-from typing import Union
-
 import mindsdb_sdk
-from mindsdb_sdk.server import Server
+from mindsdb_sdk.server import Server, Database
+from pandas import DataFrame
 from requests.exceptions import HTTPError, ConnectionError
+from rich.markdown import Markdown
 
 from hey.constants.service import MINDSDB_HOST
 from hey.exceptions.auth import CredentialsError
 from hey.exceptions.connection import NetworkError
 
 
-class User:
+def to_data(dataframe: DataFrame) -> str:
     """
-    MindsDB user auth manager class
+    takes a pandas dataframe and returns the first value from the first column
+    Args:
+        dataframe:
+
+    Returns:
+        first value of the first column of dataframe that GPT responses
     """
+    return dataframe.iloc[:, 0].values[0]
+
+
+class MindsDB:
+    """
+    MindsDB manager class
+    """
+
+    SQL_ASKING_QUERY = '''
+    SELECT response
+    FROM mindsdb.gpt_model
+    WHERE author_username = "mindsdb"
+    AND text = "{}";
+    '''
 
     def __init__(self, email: str, password: str) -> None:
         """
         initializer class.
         Args:
-            email: MindsDB account email address
+            email: MindsDB account email address (that is stored as an env var)
             password: MindsDB account password
         """
         self.email = email
         self.password = password
 
         self.is_authenticated: bool = False
+        self.database: Database
 
-    def authenticate(self) -> Union[Server, None]:
+    def authenticate(self) -> None:
         """
         authorizes the email and password with MindsDB's host
         Returns:
-            user object
+            server object
         """
+
         try:
-            user = mindsdb_sdk.connect(
+            server = mindsdb_sdk.connect(
                 MINDSDB_HOST,
                 login=self.email,
                 password=self.password,
@@ -41,7 +62,26 @@ class User:
         except HTTPError:
             raise CredentialsError('Email or password is incorrect. Make sure to enter the right credentials.')
         except ConnectionError:
-            raise NetworkError('Make sure you have access to the connection and try again.')
+            raise NetworkError('Make sure you have access to the internet and try again.')
 
         self.is_authenticated = True
-        return user
+        self.database = self.collect_database(server)
+
+    @staticmethod
+    def collect_database(server: Server) -> Database:
+        return server.list_databases()[0]
+
+    def answer(self, question: str) -> Markdown:
+        """
+        takes the question and queries then converts the response into `rich.Markdown`
+        Args:
+            question: 
+
+        Returns:
+
+        """
+        return Markdown(to_data(
+            self.database.query(
+                MindsDB.SQL_ASKING_QUERY.format(question)
+            ).fetch()
+        ))
