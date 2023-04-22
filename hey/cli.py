c4db8951dcfcd566675075d91cf1f2be11425b13
Sadra Yahyapour <lnxpylnxpy@gmail.com>
2023-04-22 18:55:16 +0330

    using rich for prints

--- hey/cli.py
+++ hey/cli.py
@@ -2,15 +2,15 @@ import argparse
 import os
 import sys
 
+import keyring
+from rich.console import Console
+
 from hey import __version__
-from hey.accounts.auth import User
 from hey.constants.informations import APPLICATION_DESCRIPTION, EPILOG_DESCRIPTION, INSTALLATION_GUIDE, VERSION_INFO
 from hey.constants.service import SERVICE_NAME
-from hey.exceptions.system import EmailEnvVarNotExists, BrokenCredentials
-from hey.validators.credentials import credentials_validator
-
-import keyring
-
+from hey.constants.system import LOCAL_EMAIL_ADDRESS_VARIABLE_NAME
+from hey.exceptions.system import BrokenCredentials, EmailEnvVarNotExists, KeyringIssue
+from hey.middlewares.mindsdb import MindsDB
 
 parser = argparse.ArgumentParser(
     description=APPLICATION_DESCRIPTION + '\n\r\n\r' + INSTALLATION_GUIDE,
@@ -19,15 +19,15 @@ parser = argparse.ArgumentParser(
 )
 
 parser.add_argument(
-    '--version',
-    action='version',
-    version=VERSION_INFO.format(__version__),
-)
-
-parser.add_argument(
     'ask',
     nargs='*',
     help='ask what you need',
+)
+
+parser.add_argument(
+    '--version',
+    action='version',
+    version=VERSION_INFO.format(__version__),
 )
 
 parser.add_argument(
@@ -40,34 +40,47 @@ parser.add_argument(
 
 def main():
     args = parser.parse_args()
+    console = Console()
 
     if args.set_password:
-        email_address = os.environ.get('HEY_EMAIL_ADDRESS')
+        email_address = os.environ.get(LOCAL_EMAIL_ADDRESS_VARIABLE_NAME)
         if email_address:
-            keyring.set_password(
-                service_name=SERVICE_NAME.lower(),
-                username=email_address,
-                password=args.set_password,
-            )
+            try:
+                keyring.set_password(
+                    service_name=SERVICE_NAME.lower(),
+                    username=email_address,
+                    password=args.set_password,
+                )
+            except Exception as _:
+                raise KeyringIssue(
+                    'There is something wrong with your OS keyring system. Make sure you have right access to run hey '
+                    'on your system. '
+                )
+            console.print(f'Password successfully set for {email_address}!')
         else:
-            raise EmailEnvVarNotExists('Make sure you have defined HEY_EMAIL_ADDRESS environment variable properly.')
+            raise EmailEnvVarNotExists(f'Make sure you have defined {LOCAL_EMAIL_ADDRESS_VARIABLE_NAME} environment '
+                                       f'variable properly.')
 
     credentials = keyring.get_credential(
         service_name=SERVICE_NAME.lower(),
-        username=os.environ.get('HEY_EMAIL_ADDRESS'),
+        username=os.environ.get(LOCAL_EMAIL_ADDRESS_VARIABLE_NAME),
     )
 
     if not credentials:
         raise BrokenCredentials(
-            'Make sure you have set your HEY_EMAIL_ADDRESS and password via --set-password.'
+            f'Make sure you have set your {LOCAL_EMAIL_ADDRESS_VARIABLE_NAME} and password via --set-password.'
         )
 
     if args.ask:
-        # TODO: Refactor User class to Connection class
-        user = User(
+        instance = MindsDB(
             email=credentials.username,
             password=credentials.password
         )
+        instance.authenticate()
+
+        console.print(instance.answer(
+            ' '.join(args.ask)
+        ))
 
     return 0
 
