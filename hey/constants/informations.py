9b3cb9433b2b0df9d798635a1bfefafcd8fc3034
Sadra Yahyapour <lnxpylnxpy@gmail.com>
2023-04-22 19:31:18 +0330

    prefix symbols removed

--- hey/constants/informations.py
+++ hey/constants/informations.py
@@ -2,11 +2,11 @@
 APPLICATION_DESCRIPTION = 'Hey is your AI-powered pair programming friend that helps you speed up your productivity.'
 
 # link to installation guide
-INSTALLATION_GUIDE = '[-] Check out https://imsadra/introducing-hey-your-ai-powered-pair-programming-friend/ for ' \
+INSTALLATION_GUIDE = 'Check out https://imsadra/introducing-hey-your-ai-powered-pair-programming-friend/ for ' \
                      'installation guide. '
 
 # epilog information
-EPILOG_DESCRIPTION = '[-] Hey is designed for Hashnode X MindsDB hackathon.'
+EPILOG_DESCRIPTION = 'Hey is designed for Hashnode X MindsDB hackathon.'
 
 # `--version` option output pattern
 VERSION_INFO = '''
