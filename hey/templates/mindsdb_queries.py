ae1deab2815a10064df7742a4be0fdf4be2570d7
Sadra Yahyapour <lnxpylnxpy@gmail.com>
2023-04-24 12:14:09 +0330

    username added

--- hey/templates/mindsdb_queries.py
+++ hey/templates/mindsdb_queries.py
@@ -3,6 +3,6 @@ from string import Template
 SQL_ASK_QUERY = Template('''
 SELECT response
 FROM mindsdb.gpt_model
-WHERE author_username = "mindsdb"
+WHERE author_username = "$user"
 AND text = "$ask";
 ''')
